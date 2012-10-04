e2f11ade65ed61255a4bb66e877612c55afb23de
thunderbump <thunderbump@gmail.com>
2012-10-04 15:14:49 -0700

    translation was swapping significancy of the digits when calculating the weight. still doesn't work though

--- 05_prot/prot.py
+++ 05_prot/prot.py
@@ -12,20 +12,22 @@ Translates an RNA sequence from acids to codons. If no data file is given,
 prot.py reads the sequence from stdin(terminated by \\n)"""
 
 def acid_translate(acid_string):
+	if(acid_string[-1] == '\n'):
+		acid_string = acid_string[:-1]
 	if(len(acid_string) % 3 != 0):
 		print("acids must be in groups of 3(len % 3 = 0)")
 		sys.exit(1)
 	translated_string = ""
 	current_char_weight = 0
-	current_char_progress = 1
+	current_char_progress = 3
 	for char in acid_string:
 		current_char_weight += (acid_weights[char] * 4 *
 				        current_char_progress)
-		current_char_progress += 1
-		if(current_char_progress == 3):
+		current_char_progress -= 1
+		if(current_char_progress == 1):
 			translated_string += weight_translation[
 						current_char_weight]
-			current_char_progress = 0
+			current_char_progress = 3
 			current_char_weight = 0
 	return translated_string
 
