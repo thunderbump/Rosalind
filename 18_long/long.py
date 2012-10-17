83543a40c0f75f68901d7aee44f45a7bcb1963ac
thunderbump <thunderbump@gmail.com>
2012-10-17 10:34:06 -0700

    stupid \r... finished long.py and updated readme

--- 18_long/long.py
+++ 18_long/long.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+"""Computes the shortest superstring of a series of substrings in a given datafile"""
 
 import sys
 
@@ -29,17 +30,6 @@ except IOError, error:
     sys.exit(1)
 
 def check_collision(seqA, seqB):
-#    print seqA, "\n", len(seqA)
-#    print seqB, "\n", len(seqB)
-#    print seqA[:516]
-#    print seqB[999-516:]
-#    for char in seqB[999-516:]:
-#        print "%r %s" % (char, char)
-#    for index, char in enumerate(seqB[999-516:]):
-#        print char, seqA[index]
-#        if seqA[index] != char:
-#            print index, seqA[index], '-', char
-#    print seqA[:516] == seqB[999-516:]
     if seqA in seqB:
         return seqB
     if seqB in seqA:
@@ -51,30 +41,23 @@ def check_collision(seqA, seqB):
         if seqB[:index] == seqA[len(seqA) - index:]:
             return "%s%s" % (seqA[:len(seqA) - index], seqB)
         index -= 1
-#    print "collision not found" , seqA,"\n", seqB
 
 def find_collision(sequences):
     primary = sequences.pop(0)
     min_len_diff = sys.maxint
     min_len_index = None
     min_len_seq = None
-#    if None in sequences:
-#        sequences.remove(None)
     for index, sequence in enumerate(sequences):
-#        print index
         candidate = check_collision(primary, sequence)
         if candidate == None:
-#            print "--", index
             continue
         vanilla_len = max(len(sequence), len(primary))
         post_len = len(candidate)
         if min_len_diff > (post_len - vanilla_len):
-#            print "#", index
             min_len_diff = (post_len - vanilla_len)
             min_len_index = index
             min_len_seq = candidate
     if min_len_seq == None:
-#        print "No collision found for %s" % primary[:20]
         min_len_sequence = primary
         sequences.append("")
         min_len_index = -1
@@ -82,39 +65,6 @@ def find_collision(sequences):
     sequences.append(min_len_seq)
     return sequences
 
-#def find_collision(sequences):
-#    primary = sequences.pop(0)
-#    print "primary", primary
-#    candidates = []
-#    candidate_parent = []
-#    print sequences
-#    for sequence in sequences:
-#        candidates.append(check_collision(primary, sequence))
-#        candidate_parent.append(sequence)
-#    print "candidates:", candidates
-#    longest_candidate_length = 0
-#    longest_candidate_parent = None
-#    longest_candidate = None
-#    for cand_index, candidate in enumerate(candidates):
-#        print candidate
-#        if candidate == None:
-#            continue
-#        if len(candidate) > longest_candidate:
-#            longest_candidate_length = len(candidate)
-#            longest_candidate_parent = candidate_parent[cand_index]
-#            longest_candidate = candidate
-#    print sequences, len(sequences)
-#    #if longest_candidate_parent:
-#    print longest_candidate_parent
-#    sequences.remove(longest_candidate_parent)
-#    sequences.append(longest_candidate)
-#    
-#    return sequences
-
-#for seq in sub_seqs:
-#print check_collision(sub_seqs[0], sub_seqs[7])
-
 while len(sub_seqs) > 1:
-#    print len(sub_seqs)
     sub_seqs = find_collision(sub_seqs)
 print sub_seqs[0]
--- README.md
+++ README.md
@@ -3,30 +3,35 @@ Rosalind
 
 http://rosalind.info/problems/as-table/
 ```
-Solved  Ex   abbv  long name
-     Y  01:   dna  Counting Nucleotides
-     Y  02:   rna  RNA Transcription
-     Y  03:  revc  Reverse Complement
-     Y  04:  hamm  Counting Point Mutations
-     Y  05:  prot  Protein Translation
-     Y  06:  subs  Finding a Motif in DNA
-     Y  07:  cons  Consensus and Profile
-     Y  08:    gc  GC Content
-     Y  09:  grph  Overlap Graphs
-     Y  10:   lcs  Finding a Shared Motif
-     Y  11:  mrna  Inferring mRNA from Protein
-     Y  12:   orf  Open Reading Frames
-     Y  13:  perm  Enumerating Gene Orders
-     Y  14:  prtm  Calculating Protein Mass
-     Y  15:  revp  Locating Restriction Sites
-     Y  16:  splc  RNA Splicing
-     Y  17:  lexf  Enumerating k-mers Lexicographically
-        18:  long  Genome Assembly as Shortest Superstring
-     Y  19:  prob  Introduction to Probability
-        20:  rear  Reversal Distance
-        21:  sign  Enumerating Oriented Gene Orderings
-        22:  sseq  Finding a Spliced Motif
-        23:  tree  Completing a Tree
-        24:  kmer  k-Mer Composition
-        25:   kmp  Speeding Up Motif Finding
+Solved  Ex     abbv  long name
+     Y  01  :   dna  Counting Nucleotides
+     Y  02  :   rna  RNA Transcription
+     Y  03  :  revc  Reverse Complement
+     Y  04  :  hamm  Counting Point Mutations
+     Y  05  :  prot  Protein Translation
+     Y  06  :  subs  Finding a Motif in DNA
+     Y  07  :  cons  Consensus and Profile
+     Y  08  :    gc  GC Content
+     Y  09  :  grph  Overlap Graphs
+     Y  10  :   lcs  Finding a Shared Motif
+     Y  11  :  mrna  Inferring mRNA from Protein
+     Y  12  :   orf  Open Reading Frames
+     Y  13  :  perm  Enumerating Gene Orders
+     Y  14  :  prtm  Calculating Protein Mass
+     Y  15  :  revp  Locating Restriction Sites
+     Y  16  :  splc  RNA Splicing
+     Y  17  :  lexf  Enumerating k-mers Lexicographically
+     Y  18  :  long  Genome Assembly as Shortest Superstring
+     Y  19  :  prob  Introduction to Probability
+        20  :  rear  Reversal Distance
+        21  :  sign  Enumerating Oriented Gene Orderings
+        22  :  sseq  Finding a Spliced Motif
+        23  :  tree  Completing a Tree
+        23.0:  corr  Error Correction in Reads
+        23.1:  eval  Introduction to Expected Value
+        23.2:  inod  Counting Phylogenetic Ancestors
+        24  :  kmer  k-Mer Composition
+        25  :   kmp  Speeding Up Motif Finding
+        26  :  lcsq  Finding a Shared Spliced Motif
+        27  :  lexv  Ordering Strings of Varying Length Lexicographically
 ```
