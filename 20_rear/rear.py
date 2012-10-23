b5845ee6844fb9aca915a6f879657998a798cd36
thunderbump <thunderbump@gmail.com>
2012-10-23 11:12:50 -0700

    dict builds seemingly correctly but now it's too big to read in in one go. have to cut the ast.literal_evals down to individual keys and go line by line i think.

--- 20_rear/rear.py
+++ 20_rear/rear.py
@@ -11,6 +11,8 @@ data_file_loc = "test_data"
 usage = """rear.py [data file]
 Finds reversal difference between sets of two permutations given in a data fi
 le"""
+base_perm = [1,2,3,4,5,6,7,8,9,10]
+build = False
 
 if argc > 2:
     print(usage)
@@ -56,17 +58,20 @@ def permute(source, path_str="", depth=0, visited={}):
     else:
         if visited[str(source)] > depth:
             visited[str(source)] = depth
-#        print("!"),
         else:
             return visited
-    if depth >= len(source) - 1:
+#        print("!"),
+        #else:
+        #    return visited
+    if depth >= len(source):
+    #if depth >= len(source) - 1:
     #if depth >= 5:
         return visited
     max_depth = len(source) - 1
-    for start_index in range(0, len(source) - 2):
+    for start_index in range(0, len(source) - 1):
         if depth < 5:
             print(" ".join([path_str, str(start_index)]))
-        for end_index in range(start_index + 2, len(source)):
+        for end_index in range(start_index + 2, len(source) + 1):
             next_step = source[:start_index]
             for item in source[start_index:end_index][::-1]:
                 next_step.append(item)
@@ -81,19 +86,27 @@ def permute(source, path_str="", depth=0, visited={}):
     return visited
         
 #print sum(a for a in source_perms[0])
-print source_perms[0], '\n', dest_perms[0]
-val = 0
-
 reference_file = open("dict_reference", 'r')
 try:
-    saved_dict = ast.literal_eval(reference_file.readline())
+#    saved_dict = ast.literal_eval(reference_file.readline())
+    saved_dict = eval(reference_file.readline())
 except:
     saved_dict = {}
 reference_file.close()
 
+if build:
+    reference_file = open("dict_reference", 'w')
+    reference_file.write(str(permute(base_perm, "", 0, saved_dict)))
+    reference_file.close()
 
-#permute(source_perms[val], "", 0, saved_dict)
-reference_file = open("dict_reference", 'w')
 
-reference_file.write(str(permute(source_perms[val], "", 0, saved_dict)))
-reference_file.close()
+for index, perm in enumerate(source_perms[1:]):
+    perm_trans = {}
+    index_trans = {}
+    for perm_index, item in enumerate(perm):
+        perm_trans[item] = perm_index + 1
+        index_trans[perm_index + 1] = item
+    #for char in dest_perms[index]:
+    #    print "%r" % char
+    #print perm_trans, index_trans
+    print(saved_dict[str(list(perm_trans[item] for item in dest_perms[index]))]),
